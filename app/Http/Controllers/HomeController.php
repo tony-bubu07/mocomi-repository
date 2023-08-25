b0f2338ff5703c64c7a08d711cd0190ff20e6920
tony-bubu07 <tony.tony.chopper.butadori@gmail.com>
2023-08-25 18:48:23 +0900

    first

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,10 +34,10 @@ class HomeController extends Controller
         $books_data = $object->orderBy('id', 'DESC')->take(10)->get();
 
         // 少年/青年
-        $men_books_data = $object->where('book_kinds_id', 1)->orderBy('id', 'DESC')->take(10)->get();
+        $men_books_data = $object->where('book_kind_id', 1)->orderBy('id', 'DESC')->take(10)->get();
 
         // 少女/女性
-        $women_books_data = $object->where('book_kinds_id', 2)->orderBy('id', 'DESC')->take(10)->get();
+        $women_books_data = $object->where('book_kind_id', 2)->orderBy('id', 'DESC')->take(10)->get();
 
         // var_dump($books_data, $men_books_data, $women_books_data);
 
